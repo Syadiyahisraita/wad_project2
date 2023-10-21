d0ca4a56336e037adf9ddb22d7e9c7ffdf55c90b
Syadiyahisraita <32837093+Syadiyahisraita@users.noreply.github.com>
2023-10-21 11:45:49 +0700

    latihan week 5 php

---
+++ latihan2.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Latihan 2 PHP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    
+  </head>
+  <body>
+    <!-- As a heading -->
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <div class="container-fluid">
+            <span class="navbar-brand mb-0 h1">Latihan 2 PHP</span>
+        </div>
+    </nav>
+    <br>
+<div class="container">
+
+<label for="inputNumber5" class="form-label">NUMBER</label>
+<input type="number" id="inputNumber5" class="form-control" aria-describedby="numberHelpBlock">
+<div id="numberHelpBlock" class="form-number">
+  Your characters contain numbers, and must not contain spaces, alfabet, special characters, or emoji.
+</div>
+<br>
+<label for="inputDropdown5" class="form-label">DROPDOWN</label>
+<select class="form-select" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+  <option value="2">Two</option>
+  <option value="3">Three</option>
+</select>
+<br>
+<a class="btn btn-primary" href="#" role="button">Hitung</a>
+
+</div>
+  </body>
+</html>
